abc2d097fc4f4714ee2e19d8100513c683be7d74
Nawarian <nawarian@gmail.com>
2022-08-24 20:38:53 +0200

    fix(menu) habilita menu "Programação"

--- source/_partials/menu.blade.php
+++ source/_partials/menu.blade.php
@@ -26,7 +26,7 @@
                     <ul class="site-menu js-clone-nav d-none">
                       <li><a href="/">Home</a></li>
                       <li class="{{ $page->selected('about') }}"><a href="/about">Sobre</a></li>
-                      <!-- <li><a href="/programming">Programação</a></li> -->
+                      <li><a href="/schedule">Programação</a></li>
                       <!-- <li><a href="https://loja.phpeste.net/produtos/index/workshop">Workshops</a></li> -->
                       <!-- <li class="{{ $page->selected('speakers') }}"><a href="/speakers">Palestrantes</a></li> -->
                       <li class="{{ $page->selected('sponsors') }}"><a href="/sponsors">Patrocinadores</a></li>
