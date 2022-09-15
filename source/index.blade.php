8c3fa3be1163cca36933f89974dd9924fd2525e4
Nawarian <nawarian@gmail.com>
2022-09-15 06:37:49 +0200

    feat(home) add ponsors

--- source/index.blade.php
+++ source/index.blade.php
@@ -31,8 +31,9 @@
     </div>
 
     @include('_about')
-
+    
     @include('_shared.block-images', ['image1' => 'images/img_1.jpg', 'image2' => 'images/img_2.jpg'])
-
     @include('_shared.block-images', ['image1' => 'images/img_3.jpg', 'image2' => 'images/img_4.jpg'])
+
+    @include('_shared.sponsors', ['title' => 'Patrocinadores'])
 @endsection
