d76998658ae27a88e5d571de3dca63de760a526c
Ítalo Epifânio <italoepifaniols@gmail.com>
2022-10-24 14:27:36 -0300

    Fix url on github pages

--- source/_partials/menu.blade.php
+++ source/_partials/menu.blade.php
@@ -7,7 +7,6 @@
       <div class="site-mobile-menu-body"></div>
     </div> <!-- .site-mobile-menu -->
     
-    
     <div class="site-navbar-wrap js-site-navbar bg-white">
       
       <div class="container">
@@ -15,7 +14,7 @@
           <div class="py-1">
             <div class="row align-items-center">
               <div class="col-2">
-                <h2 class="mb-0 site-logo"><a href="/">{{ $page->event_name }}</a></h2>
+                <h2 class="mb-0 site-logo"><a href="{{ url('/') }}">{{ $page->event_name }}</a></h2>
               </div>
               <div class="col-10">
                 <nav class="site-navigation text-right" role="navigation">
@@ -25,15 +24,15 @@
                      <!-- d-lg-block -->
                     <ul class="site-menu js-clone-nav d-none">
                       <li><a href="{{ $page->event_ticket }}" class="btn btn-primary px-4 py-2" style="color: #fff"  target="_blank">{{ $page->event_ticket_cta }}</a></li>
-                      <li><a href="/">Home</a></li>
-                      <li class="{{ $page->selected('about') }}"><a href="/about">Sobre</a></li>
-                      <li><a href="/schedules">Programação</a></li>
+                      <li><a href="{{ url('/') }}">Home</a></li>
+                      <li class="{{ $page->selected('about') }}"><a href="{{ url('/about') }}">Sobre</a></li>
+                      <li><a href="{{ url('/schedules') }}">Programação</a></li>
                       <!-- <li><a href="https://loja.phpeste.net/produtos/index/workshop">Workshops</a></li> -->
                       <!-- <li class="{{ $page->selected('speakers') }}"><a href="/speakers">Palestrantes</a></li> -->
-                      <li class="{{ $page->selected('sponsors') }}"><a href="/sponsors">Patrocinadores</a></li>
-                      <li class="{{ $page->selected('code-of-conduct') }}"><a href="/code-of-conduct">Código de conduta</a></li>
+                      <li class="{{ $page->selected('sponsors') }}"><a href="{{ url('/sponsors') }}">Patrocinadores</a></li>
+                      <li class="{{ $page->selected('code-of-conduct') }}"><a href="{{ url('/code-of-conduct') }}">Código de conduta</a></li>
                       <!-- <li><a href="https://www.depassaporte.com.br/phpeste" target="_blank">Passagens aéreas</a></li> -->
-                      <li class="{{ $page->selected('contact') }}"><a href="/contact">Contato</a></li>
+                      <li class="{{ $page->selected('contact') }}"><a href="{{ url('/contact') }}">Contato</a></li>
                     </ul>
                   </div>
                 </nav>
