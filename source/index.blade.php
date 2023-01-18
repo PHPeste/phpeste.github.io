0203f69e425b01cebe6ac0503c1f12e3b5c90511
Lucas Vinicius <lucasvinicius211@yahoo.com>
2023-01-18 00:08:36 -0300

    feat: Fix hero content

--- source/index.blade.php
+++ source/index.blade.php
@@ -9,18 +9,20 @@
       <div class="container">
         <div class="row align-items-center justify-content-center">
           <div class="col-md-10 text-center" data-aos="fade">
-            <h1 class="mb-4">Maior conferência PHP do Nordeste</h1>
+            <h1 class="mb-4">Maior conferência de PHP do Nordeste</h1>
             <div class="mb-5">
                 <p>
                     {{ $page->event_period }} &bullet; 
-                    <u>2022</u>
+                    <u>2023</u>
                 </p>
-                <a class="maps" href="" target="_blank">
-                    {{ $page->event_location }}
-                </a>
             </div>
             <div class="col-md-8 mx-auto text-center section-heading">
                 <div id="date-countdown" class="mb-4"></div>
+            </div>
+            <div class="mb-5">
+                <p>
+                    Inscrições em breve!
+                </p>
             </div>
             <a href="{{ $page->event_ticket }}" class="btn btn-primary btn-lg px-4 py-2">
               {{ $page->event_ticket_cta }}
